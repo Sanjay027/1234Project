ef79987001d841a8f095ba5da0779563dfd9f5b9
Sanjay027 <sanjay.gudapati27@gmail.com>
2020-11-21 15:09:14 +0530

    task changed

--- src/org/text/Day6_1.java
+++ src/org/text/Day6_1.java
@@ -24,7 +24,14 @@ public class Day6_1 {
 	 int check = str_101.compareTo(str_102);
 	 System.out.println("the status is :"+check);
 	 
+
+	 System.out.println("enter the value");
+	 String str_103=str_2.nextLine();
+	 System.out.println("enter the value is :"+str_102);
 	
+	 
+	 int check1 = str_101.compareTo(str_102);
+	 System.out.println("the status is :"+check);
 
 			
 	
